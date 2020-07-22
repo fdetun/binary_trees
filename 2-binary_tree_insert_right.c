2544480ebdb1999a67d036531dd6d584fc30346e
fdetun <fouedads@gmail.com>
2020-07-22 19:42:02 +0100

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - binary_tree_insert_left
+ * binary_tree_insert_right - binary_tree_insert_right
  * @parent: Pointer to the parent node
  * @value: value of node
  * Return: nd
@@ -12,25 +12,24 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 binary_tree_t *tmp = NULL;
 binary_tree_t *nd = NULL;
 
+if (!parent)
+return (NULL);
 nd = malloc(sizeof(binary_tree_t));
 if (!nd)
 return (NULL);
-nd->n = value;
+nd->parent = parent;
 nd->left = NULL;
 nd->right = NULL;
-if (!parent)
-{
-return (nd);
-}
-nd->parent = parent;
+nd->n = value;
+
 if (parent->right != NULL)
 {
-tmp=parent->right;
-parent->right=nd;
-nd->right=tmp;
-nd->parent=parent;
-tmp->parent=nd;
+tmp = parent->right;
+parent->right = nd;
+nd->right = tmp;
+nd->parent = parent;
+tmp->parent = nd;
 }
-parent->right=nd;
+parent->right = nd;
 return (nd);
 }
