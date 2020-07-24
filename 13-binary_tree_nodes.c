1a4320c4af877d6a0c4e08c925af2220ddcdf75a
fdetun <fouedads@gmail.com>
2020-07-24 06:20:04 +0100

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,7 +10,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 if (!tree)
 return (0);
-if (!tree->left && !tree->right)
-return (0);
+if (tree->left || tree->right)
 return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->left) + 1);
 }
