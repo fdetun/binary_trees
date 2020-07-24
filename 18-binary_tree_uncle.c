90b63bc18082a7e849eba26de08911a793e26058
fdetun <fouedads@gmail.com>
2020-07-24 22:09:14 +0100

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,9 +8,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (!node || !node->parent->parent)
-return (NULL);
-if (!node->parent)
+if (!node || !node->parent->parent || !node->parent)
 return (NULL);
 if (node->parent == node->parent->parent->right)
 return (node->parent->parent->left);
