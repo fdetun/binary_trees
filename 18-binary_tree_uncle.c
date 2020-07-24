a6b0d99bbd0c3951e9f586da31597acbc6c59580
fdetun <fouedads@gmail.com>
2020-07-24 22:24:34 +0100

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 binary_tree_t *fde = NULL;
 
-if (!node || node->parent->parent == NULL || !node->parent)
+if (!node || node->parent->parent == NULL || node->parent == NULL)
 return (NULL);
 fde = node->parent->parent;
 if (node->parent == fde->left)
